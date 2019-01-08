c5f795f4c4f8dcbefe1cfd36b9cc676a8f3c3b89
Avalos <raphael.avalos@telecom-paristech.fr>
2019-01-08 23:07:28 +0100

    _

--- predictor.py
+++ predictor.py
@@ -1,13 +1,22 @@
 import tensorflow as tf
 
 
-class DOOM_Predictor:
+class DOOM_Predictor():
     """
     DOOM Predictor implenting the paper 'Learning To Act By Predicting The Future'
     """
 
     def __init__(self, conf):
-        pass
+        self._visual_placeholder = tf.placeholder(dtype=tf.float32,
+                                                  shape=(-1, conf.arch['vis_size'], conf.arch['vis_size'], 1),
+                                                  name='visual_input')
+        self._measurement_placeholder = tf.placeholder(dtype=tf.float32,
+                                                       shape=(-1, conf.arch['meas_size']),
+                                                       name='meas_input')
+        self._goal_placeholder = tf.placeholder(dtype=tf.float32,
+                                                shape=(-1, conf.arch['num_time'], conf.arch['goal_size']))
+
+
 
     def predict(self, data):
         # data is (batch,s=image,m=measurement,g=goal)
