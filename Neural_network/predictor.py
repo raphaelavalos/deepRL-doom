6e86839935d8d26712f8ae6a63a9c05f8b38a3d8
Hugo Yeche <yeche@eurecom.fr>
2019-01-09 18:49:43 +0100

    V.1 subnets
    Change to tf.layers.conv2D plus predict function

--- Neural_network/predictor.py
+++ Neural_network/predictor.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+from subnets import perception_net, measurement_net, goal_net, expectation_net, action_net
+
 
 
 class DOOM_Predictor():
@@ -6,7 +8,7 @@ class DOOM_Predictor():
     DOOM Predictor implenting the paper 'Learning To Act By Predicting The Future'
     """
 
-    def __init__(self, conf):
+    def __init__(self, conf, parameters):
         self._visual_placeholder = tf.placeholder(dtype=tf.float32,
                                                   shape=(-1, conf.arch['vis_size'], conf.arch['vis_size'], 1),
                                                   name='visual_input')
@@ -15,12 +17,36 @@ class DOOM_Predictor():
                                                        name='meas_input')
         self._goal_placeholder = tf.placeholder(dtype=tf.float32,
                                                 shape=(-1, conf.arch['num_time'], conf.arch['goal_size']))
+        """
+        parameters keys are : 'perception','measurement','goal','expectation','action'
+        each value is a dict itself containing keys : 'filters','kernel_size','strides','fc_out'
+        each value of this dict are following:
+            filters : list of integers corresponding at the output dim of the respective convolutions
+            kernel_size : list of [kernel_width, kernel_width]
+            strides : list of [strides_width, strides_width]
+            fc_out : list of output dimensions for fully connected layers    
+        """
+        self.parameters = parameters
 
 
 
     def predict(self, data):
-        # data is (batch,s=image,m=measurement,g=goal)
-        pass
+        # data is (s=Tensor[batch_size,image_width,image_height], m=Tensor[number_measurement], g=Tensor[horizon])
+        images , measures, goals = data
+        perception_out = perception_net(images, self.parameters.get('perception'))
+        measurement_out = measurement_net(measures, self.parameters.get('measurement'))
+        goal_out =  goal_net(goals, self.parameters.get('goals'))
+        expectation_out = expectation_net(perception_out,
+                                          measurement_out,
+                                          goal_out,
+                                          self.parameters.get('expectation'))
+        action_out = action_net(perception_out,
+                                          measurement_out,
+                                          goal_out,
+                                          self.parameters.get('action'))
+
+        return expectation_out, action_out
+
 
     def optimize(self, data):
         # data is ((batch,s=image,m=measurement,g=goal), (batch, target))
--- Neural_network/subnets.py
+++ Neural_network/subnets.py
@@ -5,10 +5,10 @@ number_measurement = 3
 horizon = 6
 
 """PERCEPTION BLOCK"""
-def perception_conv(number, input, filter, strides, padding, activation=tf.nn.leaky_relu):
+def perception_conv(number, input, filters, kernel_size, strides, padding, activation=tf.nn.leaky_relu):
 
-    output_conv = tf.nn.conv2d(input, filter, strides, padding, name="perception_conv_{}".format(number))
-    b = tf.get_variable('b_conv_'.format(number), filter[-1], initializer=tf.constant_initializer(0.0))
+    output_conv = tf.layers.conv2d(input, filters, kernel_size, strides, padding, name="perception_conv_{}".format(number))
+    b = tf.get_variable('b_conv_'.format(number), filters, initializer=tf.constant_initializer(0.0))
     output = activation(output_conv, b)
 
     return output
@@ -22,101 +22,123 @@ def perception_full(number, input, output_dim):
     return output
 
 
-def perception_net(input):
+def perception_net(input,parameters):
     """"The shape are as in the paper for Basic model"""
     with tf.variable_scope("perception"):
         conv1 = perception_conv('1',
                                 input,
-                                [8, 8, 1, 32],
-                                [1, 4, 4, 1],
+                                parameters.get('filters')[0],
+                                parameters.get('kernel_size')[0],
+                                parameters.get('strides')[0],
                                 padding="SAME")
         conv2 = perception_conv('2',
                                 conv1,
-                                [4, 4, 32, 64],
-                                [1, 2, 2, 1],
+                                parameters.get('filters')[1],
+                                parameters.get('kernel_size')[1],
+                                parameters.get('strides')[1],
                                 padding="SAME")
         conv3 = perception_conv('3',
                                 conv2,
-                                [3, 3, 64, 64],
-                                [1, 1, 1, 1],
+                                parameters.get('filters')[2],
+                                parameters.get('kernel_size')[2],
+                                parameters.get('strides')[2],
                                 padding="SAME")
-        output = perception_full('1', conv3, 512)
+        output = perception_full('1', conv3, parameters.get('fc_out')[0])
 
     return output
 
 
 """MEASUREMENT BLOCK"""
-def measurement_full(number, input, output_dim):
+def measurement_full(number, input, output_dim , activation=tf.nn.leaky_relu):
 
-    output = tf.layers.dense(input, output_dim, name="measurement_full_{}".format(number))
+    output = tf.layers.dense(input, output_dim, name="measurement_full_{}".format(number), activation=activation)
 
     return output
 
 
-def measurement_net(input):
+def measurement_net(input, parameters):
     with tf.variable_scope("measurement"):
-        fc1 = measurement_full('1', input, 128)
-
-        fc2 = measurement_full('2', fc1, 128)
-
-        output = measurement_full('3', fc2, 128)
+        fc1 = measurement_full('1',
+                               input,
+                               parameters.get('fc_out')[0])
+        fc2 = measurement_full('2',
+                               fc1,
+                               parameters.get('fc_out')[1])
+        output = measurement_full('3',
+                                  fc2,
+                                  parameters.get('fc_out')[2],
+                                  activation=None)
 
     return output
 
 
 """GOAL BLOCK"""
-def goal_full(number, input, output_dim):
+def goal_full(number, input, output_dim, activation=tf.nn.leaky_relu):
 
-    output = tf.layers.dense(input, output_dim, name="goal_full_{}".format(number))
+    output = tf.layers.dense(input, output_dim, name="goal_full_{}".format(number), activation=activation)
 
     return output
 
 
-def goal_net(input):
+def goal_net(input, parameters):
     with tf.variable_scope("goal"):
         input_flat = tf.contrib.layers.flatten(input)
-
-        fc1 = goal_full('1', input_flat, 128)
-
-        fc2 = goal_full('2', fc1, 128)
-
-        output = goal_full('3', fc2, 128)
+        fc1 = measurement_full('1',
+                               input_flat,
+                               parameters.get('fc_out')[0])
+        fc2 = measurement_full('2',
+                               fc1,
+                               parameters.get('fc_out')[1])
+        output = measurement_full('3',
+                                  fc2,
+                                  parameters.get('fc_out')[2],
+                                  activation=None)
 
     return output
 
 
 """EXPECTATION BLOCK"""
-def expectation_full(number, input, output_dim):
+def expectation_full(number, input, output_dim, activation=tf.nn.leaky_relu):
 
-    output = tf.layers.dense(input, output_dim, name="expectation_full_{}".format(number))
+    output = tf.layers.dense(input, output_dim, name="expectation_full_{}".format(number), activation=activation)
 
     return output
 
 
-def expectation_net(perception_out, measurement_out, goal_out):
+def expectation_net(perception_out, measurement_out, goal_out, parameters):
     with tf.variable_scope("expectation"):
         input = tf.concat([perception_out, measurement_out, goal_out], 1)
 
-        fc1 = expectation_full('1', input, 512)
+        fc1 = expectation_full('1',
+                               input,
+                               parameters.get('fc_out')[0])
 
-        output = expectation_full('2', fc1, number_measurement * horizon)
+        output = expectation_full('2',
+                                  fc1,
+                                  number_measurement * horizon,
+                                  activation=None)
 
     return output
 
 
 """ACTION BLOCK"""
-def action_full(number, input, output_dim):
-    output = tf.layers.dense(input, output_dim, name="action_full_{}".format(number))
+def action_full(number, input, output_dim, activation=tf.nn.leaky_relu):
+    output = tf.layers.dense(input, output_dim, name="action_full_{}".format(number), activation=activation)
 
     return output
 
 
-def action_net(perception_out, measurement_out, goal_out):
+def action_net(perception_out, measurement_out, goal_out, parameters):
     with tf.variable_scope("action"):
         input = tf.concat([perception_out, measurement_out, goal_out], 1)
 
-        fc1 = expectation_full('1', input, 512)
+        fc1 = expectation_full('1',
+                               input,
+                               parameters.get('fc_out')[0])
 
-        output = expectation_full('2', fc1, number_action * number_measurement * horizon)
+        output = expectation_full('2',
+                                  fc1,
+                                  number_action * number_measurement * horizon,
+                                  activation=None)
 
     return output
